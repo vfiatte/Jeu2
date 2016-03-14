dd503411739327a284805ba1ba7d324b1229bcfb
admin <admin@macintosh>
2016-03-14 23:09:40 +0100

    Spring Task Scheduling added with a sample.

---
+++ src/main/java/streaming/service/ScheduledService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.service;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class ScheduledService {
+    
+    @Scheduled(cron = "*/10 * * * * MON-FRI")// sec min heure jour mois jourDuMois
+    public void cron(){
+        System.out.println("*** cron");
+    }
+    
+    @Scheduled(fixedDelay = 2000)// 2 sec entre la FIN du précédent et début suivant
+    public void fixedDelay() throws InterruptedException{
+        System.out.println("*** fixedDelay");
+    }
+}
--- src/main/java/streaming/spring/SpringConfig.java
+++ src/main/java/streaming/spring/SpringConfig.java
@@ -7,12 +7,12 @@ package streaming.spring;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 
 /**
@@ -24,6 +24,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 //@EnableAutoConfiguration
 @ComponentScan(basePackages = "streaming") 
 @EnableJpaRepositories(basePackages = "streaming")
+@EnableScheduling
 public class SpringConfig {
 
     @Bean
