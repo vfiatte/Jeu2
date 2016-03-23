78419eb44f347c5c067f0015d50b34f3e72e4263
vfiatte <valentin.fiatte@gmail.com>
2016-03-23 11:22:42 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/laFerme/servlet/ClassementServlet.java

--- src/main/java/streaming/spring/SpringConfig.java
+++ src/main/java/streaming/spring/SpringConfig.java
@@ -22,8 +22,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 //@SpringBootApplication
 //@EnableAutoConfiguration
-@ComponentScan(basePackages = "streaming") 
-@EnableJpaRepositories(basePackages = "streaming")
+@ComponentScan(basePackages = "jeu2") 
+@EnableJpaRepositories(basePackages = "jeu2")
 @EnableScheduling
 public class SpringConfig {
 
@@ -36,7 +36,7 @@ public class SpringConfig {
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
-        return Persistence.createEntityManagerFactory("streaming_PU");
+        return Persistence.createEntityManagerFactory("jeuPU");
     }
 
 }
